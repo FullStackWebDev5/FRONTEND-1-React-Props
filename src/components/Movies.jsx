30341bc92cd96a8d64e5ff1f08c8846f9d89f49b
AnkitKumarMishra5 <ankit5kumar14@gmail.com>
2025-03-06 22:24:26 +0530

    React: Props

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,22 +1,29 @@
 import MovieCard from "./MovieCard"
 
 function Movies() {
+  const movies = [
+    {
+      imgSrc: 'https://i.pinimg.com/736x/c5/bf/2e/c5bf2e92733fdc1f7af044b5a4b7032c.jpg'
+    },
+    {
+      imgSrc: 'https://i.pinimg.com/236x/e9/ca/e2/e9cae25b44580be1de3f904821fa851e.jpg'
+    },
+    {
+      imgSrc: 'https://rukminim2.flixcart.com/image/850/1000/jf8khow0/poster/a/u/h/small-hollywood-movie-poster-blade-runner-2049-ridley-scott-original-imaf3qvx88xenydd.jpeg?q=20&crop=false'
+    }
+  ]
+
   return (
     <div>
       <h1>Movies</h1>
       <div style={{ display: 'flex', gap: '1em' }}>
-        <MovieCard
-          imgSrc="https://i.pinimg.com/736x/c5/bf/2e/c5bf2e92733fdc1f7af044b5a4b7032c.jpg"
-          trendingNo={1}
-        />
-        <MovieCard
-          imgSrc="https://i.pinimg.com/236x/e9/ca/e2/e9cae25b44580be1de3f904821fa851e.jpg"
-          trendingNo={2}
-        />
-        <MovieCard
-          imgSrc="https://rukminim2.flixcart.com/image/850/1000/jf8khow0/poster/a/u/h/small-hollywood-movie-poster-blade-runner-2049-ridley-scott-original-imaf3qvx88xenydd.jpeg?q=20&crop=false"
-          trendingNo={3}
-        />
+        {movies.map((movie, index) => (
+          <MovieCard
+            key={index}
+            imgSrc={movie.imgSrc}
+            trendingNo={index+1}
+          />
+        ))}
       </div>
     </div>
   )
